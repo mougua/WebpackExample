0b90f494447c015f8467325e4944c2be2753d6e6
mougua <chengk0118@gmail.com>
2017-12-08 01:19:37 +0800

    第一个例子

--- app/Greeter.js
+++
@@ -1,7 +0,0 @@
-import config from './config.json'
-
-module.exports = function () {
-  const great = document.createElement('div')
-  great.textContent = config.greetText
-  return great
-}
--- app/main.js
+++ app/main.js
@@ -1,51 +1,5 @@
-require('./main.css')
-const ui = require('./main.ui')
-const $ = require('jquery')
-const XMPP = require('stanza.io')
-
-const client = XMPP.createClient({
-  jid: 'ck@localhost',
-  password: 'fucking',
-  transport: 'websocket',
-  wsURL: 'wss://localhost:7443/ws/'
-})
-client.use(require('./plugins/workgroup'))
-client.on('session:started', () => {
-  client.getRoster()
-  client.sendPresence()
-})
-
-client.on('chat', function (msg) {
-  console.log(msg)
-  if (msg.from.local !== 'ck') {
-    ui.messageAdd(false, msg.body)
+$(document).ready(
+  function () {
+    $('#container').html('hello world');
   }
-})
-
-$('#on').click(function () {
-  client.sendWorkgroupPresence('demo@workgroup.localhost', 4)
-})
-
-client.on('raw:incoming', (xml) => {
-  console.log(`[入] ${xml}`)
-})
-
-client.on('raw:outgoing', (xml) => {
-  console.log(`[出] ${xml}`)
-})
-
-$('#send').click(function () {
-  let fr = 'admin@localhost'
-  const msgbox = $('.messages')
-  const msg = msgbox.val()
-  if (!msg) return
-  ui.messageAdd(true, msg)
-  client.sendMessage({
-    type: 'chat',
-    to: fr,
-    body: msg
-  })
-  msgbox.val('')
-})
-
-client.connect()
+);
